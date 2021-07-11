dd877a3b4e49c15badf35a4049debf7813b86781
Alya Mizani <entschuldigung.itsalya@gmail.com>
2021-07-11 22:45:22 +0700

    cra my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+// import axios from "axios";
+
+function App() {
+  const spotify_client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  console.log(spotify_client_id);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
